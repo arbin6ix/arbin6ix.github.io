1283053b4014eb1992c208326e1efec2d2b934fb
Arbin_Chowdhury <arbinchowdhury@hotmail.ca>
2022-01-21 19:51:48 -0800

    updated ICE

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,7 +11,38 @@
         {
             location.href = "about.html";
         });
-     
+
+        //Step 1 - get reference to an entry point(s) (insertion / deletion)
+         let MainContent = document.getElementsByTagName("main")[0];
+         let DocumentBody = document.body;
+        //Step 2 - Create a HTML Element in memory
+         let MainParagraph = document.createElement("p");
+         let Article = document.createElement("article");
+         let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`;
+        //Step 3 - Configure new Element
+         MainParagraph.setAttribute("id", "MainParagraph");
+         MainParagraph.setAttribute("class", "mt-3");
+         let FirstString = "This is";
+         let SecondString = `${FirstString} the Main Paragraph`;
+         MainParagraph.textContent = SecondString;
+         Article.setAttribute("class", "container");
+        //Step 4 - perform insertion / deletion
+        //example of insert after (append)
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+     /*    DocumentBody.innerHTML = `
+            <div class="container">
+            <h1 class="display-1">Hello World!</h1>
+            <p class="mt-5">and... what do you think of this</p>
+            </div>
+        `; */
+        //example of insert before
+        //MainContent.before(MainParagraph);
+        //example of deletion
+        //document.getElementById("AboutUsButton").remove();
+        //AboutUsButton.remove();
+        //ES6 and HTML5 => Template Strings => "Super Strings"
     }
 
     function DisplayProductsPage()
